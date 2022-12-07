94e276dcd3b90e7b930a67d0b8d2f8986fb667ff
Alexandre Riff da Costa <alexandre.rdcosta@gmail.com>
2022-12-07 20:37:20 -0300

    Substituido chamada da classe repository pela service na Thread

--- src/main/java/controlefrete/thread/RelatorioDiarioThread.java
+++ src/main/java/controlefrete/thread/RelatorioDiarioThread.java
@@ -1,8 +1,15 @@
 package controlefrete.thread;
 
 import controlefrete.repositories.FreteRepository;
+import controlefrete.services.FreteService;
 
 public class RelatorioDiarioThread extends Thread {
+
+    private FreteService freteService;
+
+    public RelatorioDiarioThread() {
+        freteService = new FreteService();
+    }
 
     public void gerarRelatorio() {
         new RelatorioDiarioThread().start();
@@ -10,10 +17,9 @@ public class RelatorioDiarioThread extends Thread {
 
     @Override
     public void run() {
-        final long DIA_EM_MILISEGUNDOS = 10000;//86400000
+        final long DIA_EM_MILISEGUNDOS = 86400000;
         while (true) {
-            FreteRepository instanciaFreteRepository = FreteRepository.getInstance();
-            System.out.println(instanciaFreteRepository.buscarTudo().toString());
+            System.out.println(freteService.buscarTudo().toString());
             try {
                 sleep(DIA_EM_MILISEGUNDOS);
             } catch (InterruptedException e) {
